6f4116db6a750c7142b3b820edf869c436018da8
qq_58520590 <807056039@qq.com>
2025-09-29 06:51:29 +0800

    review

--- program_counter.v
+++ program_counter.v
@@ -7,21 +7,34 @@ input  jump;
 input [7:0] initial_addr;
 input  addr;
 output reg [7:0] pc_addr;
- 
-	always@(posedge clk or negedge rst) begin
-		if(!rst)begin 
-		   pc_addr <= initial_addr;
-		end else begin
-			if(en)begin
-			   if(jump)begin
-				    pc_addr<=addr;
-		   	end else begin
-				    pc_addr <= pc_addr+8'b0000_0001;
-				end
-			end else begin 
-			   pc_addr <= pc_addr;
-			end
-		end
+
+    always(posedge clk or negedge rst)begin
+	   if(!rst)begin
+	      pc_addr<=initial_addr;
+	   end
+	   else begin
+	      if(en)begin
+		     if(jump)begin
+                pc_addr<=addr;  
+			 end
+			 else begin
+			    pc_addr<=pc_addr+1; 
+		     end	
+		  end
+	   end
 	end
  
 endmodule
+
+
+//2025.9.29
+
+
+
+
+
+
+
+
+
+
